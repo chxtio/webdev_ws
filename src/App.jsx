1fe4d782cf93f839aa1198e85c768fe06bf74945
chxtio <chxtio@gmail.com>
2025-07-06 20:08:16 -0700

    Add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import './App.css'
+import { useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+    console.log(`Button clicked ${count + 1} times`);
+  }
+
   return (
     <>
       <h1>🎉React🎉</h1>
+      <button onClick={handleClick}>Count is {count}</button>
     </>
   )
 }
